3145b987eb530c09859010a05856b2fcc486a1be
Nancy967 <beg7520@gmail.com>
2022-07-13 09:09:29 +0800

    mockMvc

---
+++ src/test/java/com/example/demo/controller/StudentControllerTest.java
@@ -0,0 +1,59 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StudentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getById() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/students/{studentId}", 3)
+                .header("headerName", "headerValue")
+                .queryParam("grduate", "true");
+
+        MvcResult mvcResult = mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.id", equalTo(3)))
+                .andExpect(jsonPath("$.name", notNullValue()))
+                .andReturn();
+
+        String body = mvcResult.getResponse().getContentAsString();
+        System.out.println("body" + body);
+
+    }
+
+    @Test
+    public void create() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/students")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "  \"name\":\"Nancy\",\n" +
+                        "  \"score\":100,\n" +
+                        "  \"graduate\":\"false\"\n" +
+                        "}");
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(201));
+    }
+}
